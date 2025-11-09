ba15a8ac70e93e8c631b85fec746d760af5eb358
roll-raihan <raihanahmednasim@gmail.com>
2025-11-09 21:50:28 +0600

    login and register page created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl p-5">
+                <h1 className="text-5xl text-center font-bold">Login now!</h1>
+                <form className="card-body">
+                    <fieldset className="fieldset">
+                        <label className="label">Email</label>
+                        <input type="email" className="input" placeholder="Email" />
+                        <label className="label">Password</label>
+                        <input type="password" className="input" placeholder="Password" />
+                        <div><a className="link link-hover">Forgot password?</a></div>
+                        <button className="btn btn-primary mt-4">Login</button>
+                    </fieldset>
+                    <p>Don't have an account? Please, <Link to="/register" className='font-semibold text-blue-500 underline '>Register</Link></p>
+                </form>
+            </div>
         </div>
     );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Register = () => {
     return (
-        <div>
-            Register
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl p-5">
+                <h1 className="text-5xl text-center font-bold">Register now!</h1>
+                <form className="card-body">
+                    <fieldset className="fieldset">
+                        <label className="label">Email</label>
+                        <input type="email" className="input" placeholder="Email" />
+                        <label className="label">Password</label>
+                        <input type="password" className="input" placeholder="Password" />
+                        <div><a className="link link-hover">Forgot password?</a></div>
+                        <button className="btn btn-primary mt-4">Register</button>
+                    </fieldset>
+                    <p>Already have an account? Please, <Link to="/register" className='font-semibold text-blue-500 underline '>Login</Link></p>
+                </form>
+            </div>
         </div>
     );
 };
