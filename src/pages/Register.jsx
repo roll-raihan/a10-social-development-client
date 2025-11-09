e6ed80b5fe4d83246055c88dc3388c4c875f3a9e
roll-raihan <raihanahmednasim@gmail.com>
2025-11-09 21:55:54 +0600

    register page creating is done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,8 +8,12 @@ const Register = () => {
                 <h1 className="text-5xl text-center font-bold">Register now!</h1>
                 <form className="card-body">
                     <fieldset className="fieldset">
+                        <label className="label">Name</label>
+                        <input type="text" className="input" placeholder="Name" />
                         <label className="label">Email</label>
                         <input type="email" className="input" placeholder="Email" />
+                        <label className="label">Photo URL</label>
+                        <input type="text" className="input" placeholder="Photo URL" />
                         <label className="label">Password</label>
                         <input type="password" className="input" placeholder="Password" />
                         <div><a className="link link-hover">Forgot password?</a></div>
