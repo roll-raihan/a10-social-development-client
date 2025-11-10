8d0c5f834e71460621ecc957f1af1f988627d7e6
roll-raihan <raihanahmednasim@gmail.com>
2025-11-11 01:20:32 +0600

    created user account and installed sweetAlert

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../provider/AuthProvider';
+import Swal from 'sweetalert2';
 
 const Register = () => {
+    const { createUser, setUser } = use(AuthContext);
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        // const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                setUser(user);
+                Swal.fire({
+                    title: "Well Done!",
+                    text: "You created an account",
+                    icon: "success"
+                });
+            })
+            .catch(error => {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: error.message,
+                    footer: '<a href="#">Why do I have this issue?</a>'
+                });
+            })
+    }
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl p-5">
                 <h1 className="text-5xl text-center font-bold">Register now!</h1>
-                <form className="card-body">
+                <form onSubmit={handleRegister} className="card-body">
                     <fieldset className="fieldset">
                         <label className="label">Name</label>
-                        <input type="text" className="input" placeholder="Name" />
+                        <input name='name' type="text" className="input" placeholder="Name" required />
                         <label className="label">Email</label>
-                        <input type="email" className="input" placeholder="Email" />
+                        <input name='email' type="email" className="input" placeholder="Email" required />
                         <label className="label">Photo URL</label>
-                        <input type="text" className="input" placeholder="Photo URL" />
+                        <input name='photoURL' type="text" className="input" placeholder="Photo URL" required />
                         <label className="label">Password</label>
-                        <input type="password" className="input" placeholder="Password" />
+                        <input name='password' type="password" className="input" placeholder="Password" required />
                         <div><a className="link link-hover">Forgot password?</a></div>
-                        <button className="btn btn-primary mt-4">Register</button>
+                        <button type='submit' className="btn btn-primary mt-4">Register</button>
                     </fieldset>
                     <p>Already have an account? Please, <Link to="/login" className='font-semibold text-blue-500 underline '>Login</Link></p>
                 </form>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,16 +1,23 @@
 import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
+
 const AuthProvider = ({ children }) => {
 
-    const [user, setUser] = useState({
-        name: "raihan",
-        email: "raihan@nasim.com"
-    })
+    const [user, setUser] = useState(null);
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
 
     const authData = {
         user,
-        setUser
+        setUser,
+        createUser,
+        
     }
 
     return <AuthContext value={authData}>
